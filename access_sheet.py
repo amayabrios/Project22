f325b1f54de3bd69a3fe0b95c0fd712c6c184d6d
amayabrios <amaya.baderrios@gmail.com>
2023-01-27 16:58:37 -0800

    Attempt to create df updates automated, not working

--- access_sheet.py
+++ access_sheet.py
@@ -7,12 +7,13 @@ from google_auth_oauthlib.flow import InstalledAppFlow, Flow
 from google.auth.transport.requests import Request
 import os
 import pickle
+import branch1_env_pycharm as b1
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # here enter the id of your google sheet
-SAMPLE_SPREADSHEET_ID_input = os.environ['spreadsheet_id']
-SAMPLE_RANGE_NAME = 'A1:H33'
+SPREADSHEET_ID_input = os.environ['spreadsheet_id']
+RANGE_NAME = 'A1:K33'
 
 def main():
     global values_input, service
@@ -35,8 +36,8 @@ def main():
 
     # Call the Sheets API
     sheet = service.spreadsheets()
-    result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
-                                range=SAMPLE_RANGE_NAME).execute()
+    result_input = sheet.values().get(spreadsheetId=SPREADSHEET_ID_input,
+                                range=RANGE_NAME).execute()
     values_input = result_input.get('values', [])
 
     if not values_input and not values_expansion:
@@ -45,4 +46,38 @@ def main():
 
 main()
 
+# df of movie features
 df = pd.DataFrame(values_input[1:], columns=values_input[0])
+
+
+def build_df(df):
+    # loop through each row
+    for index in df.index:
+        attempts = 0
+        title = df.loc[index, "title"]
+        # not all release dates are in df, either use what is there or ask for it
+        if df.loc[index, "release_date"]:
+            release_date = df.loc[index, "release_date"]
+        else:
+            print(title)
+            release_date = input("provide release date: ")
+
+        # search API for movie details
+        search_results = b1.API_queries().search(title)
+
+        # loop through search results
+        for hit in search_results:
+            # if search result matches the movie in the df
+            if hit["release_date"][:4] == release_date:
+                headers = list(df.columns)
+                for key in hit:
+                    if key in headers:
+                        df.loc[index, key] = hit[key]
+                break
+
+        # safety net
+        attempts += 1
+        if attempts < 3:
+            if input("continue (y/n)? ") == "n":
+                return
+    return
